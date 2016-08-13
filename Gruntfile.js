f7428f55bc30cf9e5f63d5614c5afa7e8fc9c248
maishu <ansiboy@163.com>
2016-08-13 09:55:02 +0800

    创建项目

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+
+var release_dir = 'release';
+module.exports = function (grunt) {
+    var config = {
+        ts: {
+            base: {
+                src: ['src/**/*.ts'],
+                dest: release_dir,
+                options: {
+                    target: 'es5',
+                    removeComments: true,
+                    declaration: false,
+                    references: [
+                        "src/**/*.ts"
+                    ],
+                    sourceMap: false
+                }
+            }
+        }
+    };
+
+
+
+
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    grunt.registerTask('default', ['ts']);//,, 'clean'
+
+};
