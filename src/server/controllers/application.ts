3c1691f5fe7f97ab33de091d597cadfbad515391
ansiboy <ansiboy@163.com>
2016-09-05 23:07:54 +0800

    修改 应用页面

---
+++ src/server/controllers/application.ts
@@ -0,0 +1,16 @@
+import { BaseController } from './baseController'
+export class ApplicationController extends BaseController {
+    async add() {
+
+    }
+    async delete() {
+
+    }
+    async list() {
+
+    }
+    async update() {
+
+    }
+
+}
