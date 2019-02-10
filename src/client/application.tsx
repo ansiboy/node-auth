8480aaa77cd5fd577de9d67c5d45a101d6a41cec
mai.shu <ansiboy@163.com>
2019-02-10 12:37:34 +0800

    取消 setMenu

--- src/client/application.tsx
+++ src/client/application.tsx
@@ -10,26 +10,26 @@ app.error.add((sender, error, page) => {
     ui.alert({ title: '错误', message: error.message })
 })
 
-let userService = new UserService();
-userService.resources().then(resources => {
-    let menus = resources.filter(o => o.parent_id == null)
-        .map(o => ({
-            id: o.id, name: o.name, visible: o.visible,
-            path: `${o.path}?resource_id=${o.id}`
-        } as Menu))
+let userService = app.createService(UserService);
+// userService.resources().then(resources => {
+//     let menus = resources.filter(o => o.parent_id == null)
+//         .map(o => ({
+//             id: o.id, name: o.name, visible: o.visible,
+//             path: `${o.path}?resource_id=${o.id}`
+//         } as Menu))
 
-    for (let i = 0; i < menus.length; i++) {
-        menus[i].children = resources.filter(o => o.parent_id == menus[i].id)
-            .map(o => ({
-                id: o.id,
-                name: o.name,
-                path: o.path,
-                parent: menus[i],
-                visible: o.visible,
-            } as Menu))
-    }
-    app.masterPage.setMenus(menus)
-})
+//     for (let i = 0; i < menus.length; i++) {
+//         menus[i].children = resources.filter(o => o.parent_id == menus[i].id)
+//             .map(o => ({
+//                 id: o.id,
+//                 name: o.name,
+//                 path: o.path,
+//                 parent: menus[i],
+//                 visible: o.visible,
+//             } as Menu))
+//     }
+//     app.masterPage.setMenus(menus)
+// })
 
 
 class Toolbar extends React.Component<{}, { currentPageName: string }> {
