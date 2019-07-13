27265faa9fdb04223cac634485c89b507bbd5b10
Shu Mai <ansiboy@163.com>
2019-07-13 16:34:53 +0800

    update

--- out/decorators.d.ts
+++
@@ -1,4 +0,0 @@
-export declare let currentUserId: (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
-export declare let currentUser: (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
-export declare let currentTokenId: (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
-export declare let ApplicationId: (target: Object, propertyKey: string | symbol, parameterIndex: number) => void;
