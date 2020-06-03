5fe4119ec8e6b38ef7357feab30239452b426717
mai.shu <ansiboy@163.com>
2020-06-04 07:55:04 +0800

    update

--- src/user/controllers/resource.ts
+++ src/user/controllers/resource.ts
@@ -1,35 +1,12 @@
 import { errors } from "../errors";
 import { controller, routeData, action } from "maishu-node-mvc";
-import { Resource, User, ResourcePath } from "../entities";
-import { PermissionDataContext, permissionDataContext, currentUser } from "../data-context";
-import { guid } from 'maishu-toolkit';
+import { Resource } from "../entities";
+import { PermissionDataContext, permissionDataContext } from "../data-context";
 
 
 /** 资源控制器 */
 @controller("resource")
 export default class ResourceController {
-
-
-    @action()
-    async add(@permissionDataContext dc: PermissionDataContext, @currentUser user: User, @routeData { item }: { item: Resource }) {
-        // if (!item.name) throw errors.argumentFieldNull('name', 'item')
-
-        // item.id = guid()
-        // item.create_date_time = new Date(Date.now())
-
-        // if (item.sort_number == null) {
-        //     let r = await dc.resources.createQueryBuilder()
-        //         .select("max(sort_number) as max_sort_number").getRawOne();
-
-        //     item.sort_number = (r["max_sort_number"] || 0) + 100;
-        // }
-
-
-        // await dc.resources.save(item);
-        // await dc.roleResources.save({ resource_id: item.id });
-
-        // return { id: item.id, create_date_time: item.create_date_time, sort_number: item.sort_number };
-    }
 
     @action()
     async update(@permissionDataContext dc: PermissionDataContext, @routeData { item }: { item: Resource }) {
@@ -67,24 +44,4 @@ export default class ResourceController {
         return item;
     }
 
-    // @action()
-    // async set(@permissionDataContext dc: PermissionDataContext,
-    //     @routeData { resourceId, paths }: { resourceId: string, paths: string[] }) {
-    //     if (!resourceId) throw errors.argumentFieldNull("resourceId", "routeData");
-    //     if (!paths) throw errors.argumentFieldNull("pathIds", "routeData");
-
-    //     let allPaths = await dc.paths.find();
-    //     let allPathStrings = allPaths.map(o => o.value);
-    //     let notExistsPaths = paths.filter(o => allPathStrings.indexOf(o) < 0)
-    //         .map(o => ({ id: guid(), value: o, create_date_time: new Date(Date.now()) }) as Path);
-
-    //     await dc.paths.save(notExistsPaths);
-
-    //     allPaths.push(...notExistsPaths);
-
-    //     let pathIds = allPaths.filter(o => paths.indexOf(o.value) >= 0).map(o => o.id);
-    //     let resourcePaths = pathIds.map(o => ({ resource_id: resourceId, path_id: o } as ResourcePath));
-
-    //     await dc.resourcePaths.save(resourcePaths);
-    // }
 }
