bec9bf613fa18bf61764502d42a6ddcb5eede1a2
mai.shu <ansiboy@163.com>
2021-04-26 15:23:03 +0800

    添加 applicationId

--- src/gateway/entities.ts
+++ src/gateway/entities.ts
@@ -55,6 +55,9 @@ export class Role {
     @OneToMany(() => UserRole, userRole => userRole.role)
     @JoinColumn({ name: "id", referencedColumnName: "role_id" })
     userRoles?: UserRole[];
+
+    @Column({ type: "char", length: 36, nullable: true })
+    applicationId?: string;
 }
 
 @Entity("user_role")
