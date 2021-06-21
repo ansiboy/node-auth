37cb2494b4e5c777e08dd386baba7d1ffce99174
mai.shu <ansiboy@163.com>
2021-06-21 10:30:48 +0800

    添加 changePassword

--- src/user/status-codes.ts
+++ src/user/status-codes.ts
@@ -1,9 +1,10 @@
 export enum statusCodes {
-    passwordIncorect = 815,
+    usernameOrPasswordIncorect = 815,
     emailExists = 816,
     canntGetUserIdFromHeader = 817,
     verifyCodeIncorrect = 818,
     userIdNull = 819,
     mobileNotExists = 820,
-    usernameExists = 821
+    usernameExists = 821,
+    passwordIncorect = 822,
 }
